6d4afa4644053b18f018c9f8b1628cc13478e151
wuzhanhui <1521383763@qq.com>
2021-09-01 23:35:25 +0800

    ‘配置登录请求api

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,30 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
+const state = {
+  token: getToken
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async login(context, data) {
+    const result = await login(data)
+    context.commit('setToken', result) // 返回数据经过响应拦截器处理
+  }
+
+}
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,23 @@
 import axios from 'axios'
-const service = axios.create()
+import { Message } from 'element-ui'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // 配置生产和开发环境
+  timeout: 5000
+})
 service.interceptors.request() // 请求拦截器
-service.interceptors.response() // 响应拦截器
+
+// 响应拦截器
+service.interceptors.response(response => {
+  const { success, message, data } = response.data
+  if (success) {
+    return data
+  } else {
+    Message.error(message) // element-ui 提示错误消息
+  }
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(new Error(error)) // 返回执行错误 进入cath
+})
+
 export default service
